60d83e883380219b37e909d9fb6af5bf96f00a0d
Dmitry Pazuxin <dpazuxin@bk.ru>
2022-10-10 02:26:31 +0300

    movies-explorer-frontend: наладка инфраструктуры

---
+++ src/components/App/App.js
@@ -0,0 +1,7 @@
+import Header from '../Header/Header.js';
+import './App.css';
+
+function App() {  
+  return <Header />;
+}
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import './Header.css';
+
+function Header() {
+  return <input type="text" className="Header" />;
+}
+
+export default Header;
